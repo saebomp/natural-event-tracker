08aa1fbe8c3aed5d838f1d69f5d1d871609de6bd
SAEBOMPAK <saebompak@gmail.com>
2021-07-22 02:16:19 +0900

    feat: add google maps react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Map from './components/Maps'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Map></Map>
     </div>
   );
 }
 
 export default App;
+
+
+// https://www.youtube.com/watch?v=ontX4zfVqK8
+// AIzaSyDneP7oCKWNUEjS7CEc2WDEYF77nOm1Oxg
---
+++ src/components/Maps.js
@@ -0,0 +1,26 @@
+import { map } from 'async'
+import GoogleMapReact from 'google-map-react'
+
+const Map = ({center, zoom}) => {
+  return (
+    <div className="map">
+      <GoogleMapReact
+        bootstrapURLKeys={{key:'AIzaSyDneP7oCKWNUEjS7CEc2WDEYF77nOm1Oxg'}}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+
+      </GoogleMapReact>
+    
+    </div>
+  )
+}
+
+Map.defaultProps={
+  center:{
+    lat:42.3265,
+    lng:-122.8756
+  },
+  zoom:6
+}
+export default Map
