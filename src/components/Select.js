dddc6ed0c0eab32cef04a432045b5815b960c471
SAEBOMPAK <saebompak@gmail.com>
2021-08-05 01:40:05 +0900

    update form style

--- src/components/Select.js
+++ src/components/Select.js
@@ -24,7 +24,7 @@ const labels = [
   {label: "All", count: 1},
   {label: "Wildfires", count:2},
   {label: "Sea And Lake Ice", count:3},
-  {label: "Storms", count:4}
+  {label: "Storms", count:400}
 ]
 
 const Select = ({type, changeType}) => {
@@ -41,10 +41,10 @@ const Select = ({type, changeType}) => {
       <FormControl component="fieldset">
       <FormLabel component="legend" className={classes.formControl}>Type</FormLabel>
       <RadioGroup aria-label="type" className={classes.labelWrap} name="type" value={type} onChange={handleChange}>
-        <FormControlLabel value="all" control={<Radio />} label={labels[0].label} className={classes.label} /> <span>{labels[0].count}</span>
-        <FormControlLabel value="wildfires" control={<Radio />} label={labels[1].label} className={classes.label} /> <span>{labels[1].count}</span>
-        <FormControlLabel value="ice" control={<Radio />} label={labels[2].label} className={classes.label} /> <span>{labels[2].count}</span>
-        <FormControlLabel value="storms" control={<Radio />} label={labels[3].label} className={classes.label} /> <span>{labels[3].count}</span>
+        <FormControlLabel value="all" control={<Radio />} label={labels[0].label} className={classes.label} /> <span className="count">{labels[0].count}</span>
+        <FormControlLabel value="wildfires" control={<Radio />} label={labels[1].label} className={classes.label} /> <span className="count">{labels[1].count}</span>
+        <FormControlLabel value="ice" control={<Radio />} label={labels[2].label} className={classes.label} /> <span className="count">{labels[2].count}</span>
+        <FormControlLabel value="storms" control={<Radio />} label={labels[3].label} className={classes.label} /> <span className="count">{labels[3].count}</span>
       </RadioGroup>
     </FormControl>
     </div>
